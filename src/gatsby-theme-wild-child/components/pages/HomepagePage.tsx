cc80bb28b69ecb0e3577333d2d8aa326ac3bd265
andy-dextrous <marketing@andrewscrivens.com.au>
2023-02-13 11:01:57 +0000

    Update Wild-Child-Starter from andy-dextrous/WILD-Child

--- src/gatsby-theme-wild-child/components/pages/HomepagePage.tsx
+++ src/gatsby-theme-wild-child/components/pages/HomepagePage.tsx
@@ -1,9 +1,12 @@
 import { Heading, VStack } from "@chakra-ui/react"
+import { useLocalFile } from "gatsby-theme-wild-child"
 import Layout from "gatsby-theme-wild-child/src/components/Layout"
 import Section from "gatsby-theme-wild-child/src/components/Section"
 import React from "react"
 
 function HomepagePage() {
+  const data = useLocalFile("Setup-Cost-Hero")
+
   return (
     <Layout>
       <Section>
